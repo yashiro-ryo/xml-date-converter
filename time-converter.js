c4796b983a666898d22a0eabdf62ca4bcbc3c879
yashiroryo <>
2024-03-17 21:42:46 +0900

    refactor: 関数化

--- time-converter.js
+++ time-converter.js
@@ -1,43 +1,56 @@
 const xml2js = require("xml2js");
 const fs = require('fs');
 
-const FILE_NAME = '****.xml'
+const FILE_NAME = '5/05.xml'
 const START_HOUR = 0
-const START_MIN = 0
+const START_MIN = 12
 const START_SEC = 0
 
-// xmlファイル読み込み
-let xml = fs.readFileSync(FILE_NAME);
+function main() {
+  // xmlファイル読み込み
+  let xml = fs.readFileSync(FILE_NAME);
 
-// XMLパース
-xml2js.parseString(xml, function (error, result) {
-  // 変換が成功したか判定
+  // XMLパース
+  xml2js.parseString(xml, parseStringCb);
+}
+
+function parseStringCb(error, result) {
   if (error) {
-    console.log(error.message);
+    console.log(error)
   } else {
-    // 変換後のオブジェクトを出力
-    const texts = result.tt.body[0].div[0].p
-    const startMillseconds = (START_HOUR * 3600 + START_MIN * 60 + START_SEC) * 1000
-
-    console.log(texts)
-
-    // 文字列の時刻 -> ミリ秒 -> 動画のスタート位置を加算 -> 文字列の時刻に戻す
-    texts.forEach((value) => {
-      value.$.begin = millisecondsToTimeString(timeStringToMilliseconds(value.$.begin) + startMillseconds)
-      value.$.end = millisecondsToTimeString(timeStringToMilliseconds(value.$.end) + startMillseconds)
-    })
-
-    console.log(texts)
-
-    // JSONをXMLに戻す
-    const builder = new xml2js.Builder();
-    const updatedXml = builder.buildObject(result);
-
-    // 更新されたXMLをファイルに書き込む
-    fs.writeFile(`converted-${FILE_NAME}`, updatedXml, (err) =>
-      console.error(err));
+    onSuccess(result)
   }
-});
+}
+
+function onSuccess(result) {
+  // 変換後のオブジェクトを出力
+  const texts = result.tt.body[0].div[0].p
+  const startMillseconds = (START_HOUR * 3600 + START_MIN * 60 + START_SEC) * 1000
+
+  console.log(texts)
+
+  // 文字列の時刻 -> ミリ秒 -> 動画のスタート位置を加算 -> 文字列の時刻に戻す
+  texts.forEach((value) => {
+    value.$.begin = millisecondsToTimeString(timeStringToMilliseconds(value.$.begin) + startMillseconds)
+    value.$.end = millisecondsToTimeString(timeStringToMilliseconds(value.$.end) + startMillseconds)
+  })
+
+  console.log(texts)
+
+  // JSONをXMLに戻す
+  const builder = new xml2js.Builder();
+  const updatedXml = builder.buildObject(result);
+
+  // 更新されたXMLをファイルに書き込む
+  fs.writeFile(`converted-${FILE_NAME}`, updatedXml, (err) =>
+    console.error(err));
+}
+
+main()
+
+//
+// utils
+//
 
 // 時間の文字列をミリ秒に変換する関数
 function timeStringToMilliseconds(timeString) {
